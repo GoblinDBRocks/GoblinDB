1a49013a708394f1beccb19f3e69a5ce510e2ab3
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-01-26 10:24:57 +0000

    last changes in the core api

--- goblin.js
+++ goblin.js
@@ -2,41 +2,48 @@ var configGoblin = require("./config");
 var helpers = require("./goblin_helpers");
 var fs = require('fs');
 var _ = require('lodash');
-var O = require('observed')
-
-//var WatchJS = require("watchjs")
-//var watch = WatchJS.watch;
-//var Scheduled = require("scheduled");
-
+var O = require('observed');
+var randomstring = require("randomstring");
 
 /* ---- Basic Goblin Skeleton ---- */
 var goblin = {
     config: configGoblin,
     db:{},
     hooks: {
-        onRecordChanges: undefined
+        add: null,
+        remove: null,
+        repositoy: []
     },
     saveDataTask: undefined
 };
 
-/* ---- Goblin Internal Fucntions ---- */
+/* ---- Goblin Internal Functions ---- */
 
-
+goblin.hooks.add = function(event, callback){
+    if(event && typeof(event) === "string" && callback && typeof(callback) === "function"){
+        goblin.hooks.repositoy.push({"event": event, "callback": callback});
+    } else {
+        throw configGoblin.logPrefix, 'Event Error Record. Check your arguments:';
+    }
+}
+goblin.hooks.remove = function(event, callback){
+    _.remove(goblin.hooks.repositoy, {"event": event, "callback": callback});
+}
 
 /* ---- Goblin Internal Events + Hooks Execution ---- */
 
 var O = require('observed')
-var ee = O(goblin)
+var eventEmitter = O(goblin)
 
 //ee.on('change', function(){console.log("aaaaa")})
 
 
 //console.log("holaaa...")
-ee.on('change', function(changes){
-    //console.log("cambios detectados...", changes)
+eventEmitter.on('change', function(changes){
     var err = false
 
     if (changes.path === "db") {
+        // Recording data in the file
         if(goblin.config.recordChanges){
             fs.writeFile(goblin.config.file, JSON.stringify(goblin.db), function(error) {
                 if(error) {
@@ -45,17 +52,18 @@ ee.on('change', function(changes){
                 }
             });
         }
-    
-        if(goblin.hooks.onRecordChanges && typeof(goblin.hooks.onRecordChanges) === "function" ){
-            err ? goblin.hooks.onRecordChanges(err) : goblin.hooks.onRecordChanges();
-        }
+        
+        // Hooks management
+        goblin.hooks.repositoy.forEach(function(hook){
+            if(hook.event ===  changes.type || hook.event === "change"){
+                hook.callback({"value": changes.value, "oldValue": changes.oldValue})
+            }
+        })
+
     } else {
         // changes in goblin (as objects) not tracket in this version
     }
-})
-
-goblin.db = {"aaa": "bbb"}
-
+});
 
 /* ---- Goblin Module Exportation ---- */
 
@@ -72,10 +80,12 @@ module.exports = function(config){
     }
     
     return {
+        on: goblin.hooks.add,
+        off: goblin.hooks.remove,
         getConfig: function(){
             return goblin.config;
         },
-        setConfig: function (data){
+        updateConfig: function (data){
             goblin.config = _.merge({}, goblin.config, data);
         },
         stopStorage: function () {
@@ -84,18 +94,35 @@ module.exports = function(config){
         startStorage: function () {
             goblin.config.recordChanges = true;
         },
-        get: function () {
-            return goblin.db
+        get: function (point) {
+            if(point && typeof(point) === "string"){
+                return goblin.db[point];
+            } else {
+                return goblin.db;
+            }
         },
-        set: function(data){
-            if(typeof(data) === "object"){
-                goblin.db = data;
+        push: function (data){
+            var newKey = randomstring.generate();
+            
+            if(data && typeof(data) === "object"){
+                goblin.db[newKey] = data;
             } else {
                 throw configGoblin.logPrefix, 'Database saving error: no data provided or data is not an object/Array.';
             }
         },
-        update: function(data){
-            if(typeof(data) === "object"){
+        set: function(data, point){
+            if(point && typeof(point) === "string" && data && typeof(data) === "object"){
+                goblin.db[point] = data;
+            } else if (!point && data && typeof(data) === "object"){
+                goblin.db = data;
+            } else {
+                throw configGoblin.logPrefix, 'Database saving error: no data provided or data is not an object/Array.';
+            };
+        },
+        update: function(data, point){
+            if(point && typeof(point) === "string" && typeof(data) === "object"){
+                goblin.db[point] = _.merge({}, goblin.db, data);
+            } else if (!point && typeof(data) === "object"){
                 goblin.db = _.merge({}, goblin.db, data);
             } else {
                 throw configGoblin.logPrefix, 'Database saving error: no data provided or data is not an object/Array.';
